6a9cc931725040a3f684b8d42ecce081ad221714
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-07-09 23:57:09 +0530

    react app

---
+++ day1/myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Components/Todo';
+import Input from './Components/Input';
+
+function App() {
+  return (
+   
+  //  <Todo/>
+   <Input/>
+  );
+}
+
+export default App;
---
+++ day1/myapp/src/Components/Input.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+export default function Input() {
+
+    const [text, setText] = useState('')
+
+    function handleChange(event) {
+        setText(event.target.value)
+    }
+
+    return (
+        <div>
+            <input value={text}
+                onChange={handleChange}
+            />
+            <button>Add</button>
+            <p>{text}</p>
+        </div>
+    )
+}
